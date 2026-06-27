6aded829c3385205d9efc27ba0579ce70ca1ad64
Abhi . <abhidubey2129@gmail.com>
2026-06-27 18:34:37 +0530

    add new method

--- src/main/java/com/docker_network/controller/RegistrationController.java
+++ src/main/java/com/docker_network/controller/RegistrationController.java
@@ -6,12 +6,17 @@ import com.docker_network.entity.Registration;
 import com.docker_network.service.RegistrationService;
 
 @RestController
-@RequestMapping("/controller")
+@RequestMapping
 public class RegistrationController {
 
 	private RegistrationService registrationService;
 	public RegistrationController(RegistrationService registrationService) {
 		this.registrationService= registrationService;
+	}
+	
+	@GetMapping
+	public String welcomeInOrg() {
+		return "WELCOME TO OUR ORGANIZATION!";
 	}
 	
 	@PostMapping("/register")
